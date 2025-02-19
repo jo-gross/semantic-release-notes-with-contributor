fb238b9b2ca0fc04326eba631eba085bab54f778
babblebey <babblebey@gmail.com>
2025-02-19 11:56:32 +0100

    feat: add config resolution for Octokit integration

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-import { SemanticReleaseOctokit } from "./lib/octokit.js";
+import resolveConfig from "./lib/resolve-config.js";
+import { SemanticReleaseOctokit, toOctokitOptions } from "./lib/octokit.js";
 
 export async function analyzeCommits(pluginConfig, context, { Octokit = SemanticReleaseOctokit } = {}) {
-  
+  const {
+    githubToken,
+    githubUrl,
+    githubApiPathPrefix,
+    githubApiUrl,
+    proxy,
+    ...options
+  } = resolveConfig(pluginConfig, context);
+
+  // Create Octokit instance
+  const octokit = new Octokit(
+    toOctokitOptions({
+      githubToken,
+      githubUrl,
+      githubApiPathPrefix,
+      githubApiUrl,
+      proxy,
+    }),
+  );
 }
